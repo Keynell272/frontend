13338cdefae3db6c3e3829ac6ee8f28200cac165
Keynell <keynell272@gmail.com>
2025-10-12 17:43:04 -0600

    Elimine la carpeta control y persistencia

--- src/main/java/proyecto/main/Main.java
+++ src/main/java/proyecto/main/Main.java
@@ -1,17 +1,11 @@
 package proyecto.main;
 
 import javax.swing.SwingUtilities;
-
-import proyecto.control.ControlLogin;
 import proyecto.view.VentanaLogin;
 
 
 public class Main {
     public static void main(String[] args) {
-        ControlLogin controlLogin = new ControlLogin();
-
-        SwingUtilities.invokeLater(() -> {
-            new VentanaLogin(controlLogin).setVisible(true);
-        });
+        
     }
 }
